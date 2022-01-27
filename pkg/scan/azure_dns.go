3d631ec2193ada1662cc68b820227f50d3612032
Andrew Stuart <andrew.stuart2@gmail.com>
2022-01-27 16:07:43 -0700

    Add naive azure DNS provider.

--- pkg/scan/azure_dns.go
+++ pkg/scan/azure_dns.go
@@ -1,9 +1,103 @@
 package scan
 
-import "github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2017-10-01/dns"
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dns/armdns"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
 
 type AzureDNSScanner struct {
-	dns.BaseClient
+	Creds        azcore.TokenCredential // Usually fulfilled via azidentity credential
+	Subscription string
+}
+
+func (a *AzureDNSScanner) Scan(ctx context.Context) (Certer, error) {
+	groups := armresources.NewResourceGroupsClient(a.Subscription, a.Creds, nil)
+	zoneCli := armdns.NewZonesClient(a.Subscription, a.Creds, nil)
+	rsCli := armdns.NewRecordSetsClient(a.Subscription, a.Creds, nil)
+
+	pg := groups.List(nil)
+
+	cc := NewChanCerter()
+
+	go func() {
+		for pg.NextPage(ctx) {
+			for _, g := range pg.PageResponse().Value {
+				pg := zoneCli.ListByResourceGroup(*g.Name, &armdns.ZonesClientListByResourceGroupOptions{})
+
+				for pg.NextPage(ctx) {
+					for _, dns := range pg.PageResponse().Value {
+						pg := rsCli.ListByDNSZone(*g.Name, *dns.Name, nil)
+						for pg.NextPage(ctx) {
+							for _, rec := range pg.PageResponse().Value {
+								if rec.Properties.CnameRecord != nil {
+									ips, err := net.LookupIP(*rec.Properties.CnameRecord.Cname)
+									if err != nil {
+										cc.errs <- fmt.Errorf("error looking up cname ips: %w", err)
+									}
+									for _, ip := range ips {
+										sc := IPScanner{
+											IP:      ip,
+											Ports:   []int16{443},
+											Timeout: 10 * time.Second,
+											Scans: []ConnScanner{&HandshakeScanner{
+												Configs: []*tls.Config{{
+													InsecureSkipVerify: true,
+													ServerName:         *rec.Name,
+												}},
+											}},
+										}
+										crt, err := sc.Scan(ctx)
+										if err != nil {
+											cc.errs <- fmt.Errorf("error scanning: %w", err)
+											continue
+										}
+										go crt.Read(ctx, cc.certs, cc.errs)
+									}
+								}
+								for _, aRec := range rec.Properties.ARecords {
+									ip := net.ParseIP(*aRec.IPv4Address)
+									sc := IPScanner{
+										IP:      ip,
+										Ports:   []int16{443},
+										Timeout: 10 * time.Second,
+										Scans: []ConnScanner{&HandshakeScanner{
+											Configs: []*tls.Config{{
+												InsecureSkipVerify: true,
+												ServerName:         *rec.Name,
+											}},
+										}},
+									}
+									crt, err := sc.Scan(ctx)
+									if err != nil {
+										cc.errs <- fmt.Errorf("error scanning: %w", err)
+										continue
+									}
+									go crt.Read(ctx, cc.certs, cc.errs)
+								}
+							}
+						}
+						if pg.Err() != nil {
+							cc.errs <- fmt.Errorf("error paging dns records: %w", pg.Err())
+						}
+					}
+				}
+				if pg.Err() != nil {
+					cc.errs <- fmt.Errorf("error paging dns zones: %w", pg.Err())
+				}
+			}
+			if pg.Err() != nil {
+				cc.errs <- fmt.Errorf("error paging resourcegroups: %w", pg.Err())
+			}
+		}
+	}()
+	return cc, nil
 }
 
 // for rec := range recs {
